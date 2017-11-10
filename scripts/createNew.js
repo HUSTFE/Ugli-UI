3a2105b07415fea41a9c10caf4c14e01bbb3369a
MartinNey <nielumartin@hotmail.com>
2017-11-10 20:31:53 +0800

    style: change json format

--- scripts/createNew.js
+++ scripts/createNew.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // TODO: make render more pure
 
 const yargs = require('yargs')
@@ -5,29 +6,62 @@ const art = require('art-template')
 const fs = require('fs')
 const path = require('path')
 
+const withLog = msg => (func) => {
+  // console.log(`[-] ${msg}`)
+  // TODO: handle promise
+  try {
+    func()
+    console.log(`[√] ${msg}`)
+    return true
+  } catch (e) {
+    console.log(e)
+    console.log(`[×] ${msg}`)
+  }
+  return false
+}
+
 const firstUpperCase = ([first, ...rest]) => first.toUpperCase() + rest.join('')
-const mkdir = p => fs.existsSync(p) || fs.mkdirSync(path.resolve(p))
-const write = object => input => p => fs.existsSync(p) || fs.writeFileSync(path.resolve(p), art(path.resolve(input), object))
-const every = (arr, func) => { for (let i = 0; i < arr.length; i += 1) { func(i) } }
+const mkdir = p => fs.existsSync(p) || fs.mkdirSync(p)
+const write = object => i => o => fs.existsSync(o) || fs.writeFileSync(o, art(i, object))
+const readOneStringOrTwo = feed => (typeof feed === 'string' ? [feed, feed] : feed)
+
 const type = yargs.argv._[0]
 const name = firstUpperCase(yargs.argv._[1])
+
 const templateSrc = `./scripts/template/${type}`
 const config = JSON.parse(fs.readFileSync(`${templateSrc}/config.json`, 'utf8'))
 
 const render = c => (info) => {
   const writeWith = write(info)
+  // we need function writeWith, so can't put this outside.
+  const createFileGroup = (inputDir, outputDir, template) => {
+    const [iDirPath, oDirPath] = [inputDir, outputDir].map(rawPath => path.resolve(rawPath))
+    return withLog(`Dir ${oDirPath}`)(() => mkdir(oDirPath))
+      && template.reduce((acc, filename) => {
+        const [iFile, oFile] = readOneStringOrTwo(filename)
+        const [iPath, oPath] = [path.resolve(iDirPath, iFile), path.resolve(oDirPath, oFile)]
+        return withLog(`File ${oPath}`)(
+          () => (writeWith(iPath)(oPath) && acc)
+        )
+      }, true)
+  }
+  const createMulti = ([{ dir, template }, ...other], ...arg) => {
+    const [iDir, oDir] = readOneStringOrTwo(dir)
+    const [iBase, oBase] = arg
+    if (!createFileGroup(`${iBase}/${iDir}`, `${oBase}/${oDir}`, template)) {
+      return false
+    }
+    return other.length ? createMulti(other, ...arg) : true
+  }
+  // style template and output path
+  const [iDir, oDir] = [path.resolve('./scripts/template/'), path.resolve(`./src/style/${info.name}`)]
 
-  every(c.dir, i => mkdir(`${c.base}${info.name}${c.dir[i]}`))
-  every(c.file, i => writeWith(`${templateSrc}${c.file[i][0]}`)(`${c.base}${info.name}${c.file[i][1]}`))
-
-  mkdir(`./src/style/${info.name}`)
-  writeWith('./scripts/template/index.sass')(`./src/style/${info.name}/index.sass`)
+  createMulti(c.files, templateSrc, `${c.base}/${info.name}`)
+  withLog(`Style dir & file: ${oDir}`)(
+    () => createFileGroup(iDir, oDir, [['index.sass', 'index.sass']])
+  )
 }
 
-try {
-  render(config)({ name, type })
-} catch (e) {
-  console.log(e, '\n', '[X] create error')
-}
-
-console.log('[-] create end')
+withLog(
+  'Main'
+)(() => render(config)({ name, type }))
