0b59b77b56a82cf17ba3a6ee182ab11eaddc85a0
faraway <hzylovelyl@gmail.com>
2017-11-10 22:12:25 +0000

    feat: add colorful console log in createNew.js

--- scripts/createNew.js
+++ scripts/createNew.js
@@ -5,17 +5,18 @@ const yargs = require('yargs')
 const art = require('art-template')
 const fs = require('fs')
 const path = require('path')
+const chalk = require('chalk')
 
 const withLog = msg => (func) => {
   // console.log(`[-] ${msg}`)
   // TODO: handle promise
   try {
     func()
-    console.log(`[√] ${msg}`)
+    console.log(`${chalk.bold.green('[√]')} ${msg}`)
     return true
   } catch (e) {
     console.log(e)
-    console.log(`[×] ${msg}`)
+    console.log(`${chalk.bold.red('[×]')} ${msg}`)
   }
   return false
 }
@@ -36,11 +37,11 @@ const render = c => (info) => {
   // we need function writeWith, so can't put this outside.
   const createFileGroup = (inputDir, outputDir, template) => {
     const [iDirPath, oDirPath] = [inputDir, outputDir].map(rawPath => path.resolve(rawPath))
-    return withLog(`Dir: ${oDirPath}`)(() => mkdir(oDirPath))
+    return withLog(`Dir: ${chalk.grey(oDirPath)}`)(() => mkdir(oDirPath))
       && template.reduce((acc, filename) => {
         const [iFile, oFile] = readOneStringOrTwo(filename)
         const [iPath, oPath] = [path.resolve(iDirPath, iFile), path.resolve(oDirPath, oFile)]
-        return withLog(`File: ${oPath}`)(
+        return withLog(`File: ${chalk.grey(oPath)}`)(
           () => (writeWith(iPath)(oPath) && acc)
         )
       }, true)
@@ -57,7 +58,7 @@ const render = c => (info) => {
   const [iDir, oDir] = [path.resolve('./scripts/template/'), path.resolve(`./src/style/${info.name}`)]
 
   createMulti(c.files, templateSrc, `${c.base}/${info.name}`)
-  withLog(`Style dir & file: ${oDir}`)(
+  withLog(`Style dir & file: ${chalk.grey(oDir)}`)(
     () => createFileGroup(iDir, oDir, [['index.sass', 'index.sass']])
   )
 }
