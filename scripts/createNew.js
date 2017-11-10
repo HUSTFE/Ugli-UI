84e3c8278aeb444c2707d38dc244645ef277c467
mxz96102 <mxz96102@qq.com>
2017-11-10 10:50:42 +0800

    opti: abstract array loop

--- scripts/createNew.js
+++ scripts/createNew.js
@@ -8,7 +8,7 @@ const path = require('path')
 const firstUpperCase = ([first, ...rest]) => first.toUpperCase() + rest.join('')
 const mkdir = p => fs.existsSync(p) || fs.mkdirSync(path.resolve(p))
 const write = object => input => p => fs.existsSync(p) || fs.writeFileSync(path.resolve(p), art(path.resolve(input), object))
-
+const every = (arr, func) => { for (let i = 0; i < arr.length; i += 1) { func(i) } }
 const type = yargs.argv._[0]
 const name = firstUpperCase(yargs.argv._[1])
 const templateSrc = `./scripts/template/${type}`
@@ -16,17 +16,9 @@ const config = JSON.parse(fs.readFileSync(`${templateSrc}/config.json`, 'utf8'))
 
 const render = c => (info) => {
   const writeWith = write(info)
-  let l = c.dir.length
 
-  for (let i = 0; i < l; i += 1) {
-    mkdir(`${c.base}${info.name}${c.dir[i]}`)
-  }
-
-  l = c.file.length
-
-  for (let i = 0; i < l; i += 1) {
-    writeWith(`${templateSrc}${c.file[i][0]}`)(`${c.base}${info.name}${c.file[i][1]}`)
-  }
+  every(c.dir, i => mkdir(`${c.base}${info.name}${c.dir[i]}`))
+  every(c.file, i => writeWith(`${templateSrc}${c.file[i][0]}`)(`${c.base}${info.name}${c.file[i][1]}`))
 
   mkdir(`./src/style/${info.name}`)
   writeWith('./scripts/template/index.sass')(`./src/style/${info.name}/index.sass`)
@@ -35,7 +27,7 @@ const render = c => (info) => {
 try {
   render(config)({ name, type })
 } catch (e) {
-  console.log(e)
-  console.log('[X] create error')
+  console.log(e, '\n', '[X] create error')
 }
+
 console.log('[-] create end')
