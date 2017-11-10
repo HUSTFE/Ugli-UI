609062860568cd597545ede09879c7db838f576e
mxz96102 <mxz96102@qq.com>
2017-11-10 10:08:44 +0800

    opti: simplify new command method

--- scripts/createNew.js
+++ scripts/createNew.js
@@ -1,4 +1,4 @@
-// TODO to simplify this operation
+// TODO: make render more pure
 
 const yargs = require('yargs')
 const art = require('art-template')
@@ -6,26 +6,36 @@ const fs = require('fs')
 const path = require('path')
 
 const firstUpperCase = ([first, ...rest]) => first.toUpperCase() + rest.join('')
+const mkdir = p => fs.existsSync(p) || fs.mkdirSync(path.resolve(p))
+const write = object => input => p => fs.existsSync(p) || fs.writeFileSync(path.resolve(p), art(path.resolve(input), object))
 
 const type = yargs.argv._[0]
 const name = firstUpperCase(yargs.argv._[1])
+const templateSrc = `./scripts/template/${type}`
+const config = JSON.parse(fs.readFileSync(`${templateSrc}/config.json`, 'utf8'))
 
+const render = c => (info) => {
+  const writeWith = write(info)
+  let l = c.dir.length
 
-const mkdir = p => fs.mkdirSync(path.resolve(p))
-const write = object => input => p => fs.writeFileSync(path.resolve(p), art(path.resolve(input), object))
+  for (let i = 0; i < l; i += 1) {
+    mkdir(`${c.base}${info.name}${c.dir[i]}`)
+  }
 
-const writeReact = write({ name })
+  l = c.file.length
 
-mkdir(`./src/components/${type}/${name}`)
-mkdir(`./src/components/${type}/${name}/__test__`)
-mkdir(`./src/components/${type}/${name}/__test__/__snapshots__`)
-writeReact('./scripts/template/react/index.art')(`./src/components/${type}/${name}/index.js`)
-writeReact('./scripts/template/react/__tests__/index.art')(`./src/components/${type}/${name}/__test__/index.js`)
-writeReact('./scripts/template/react/__tests__/__snapshots__/index.art.snap')(`./src/components/${type}/${name}/__test__/__snapshots__/index.js.snap`)
+  for (let i = 0; i < l; i += 1) {
+    writeWith(`${templateSrc}${c.file[i][0]}`)(`${c.base}${info.name}${c.file[i][1]}`)
+  }
 
+  mkdir(`./src/style/${info.name}`)
+  writeWith('./scripts/template/index.sass')(`./src/style/${info.name}/index.sass`)
+}
 
-mkdir(`./src/style/${name}`)
-writeReact('./scripts/template/index.sass')(`./src/style/${name}/index.sass`)
-
-
-
+try {
+  render(config)({ name, type })
+} catch (e) {
+  console.log(e)
+  console.log('[X] create error')
+}
+console.log('[-] create end')
