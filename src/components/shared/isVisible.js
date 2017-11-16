5d6a42928c99d76d0c7fa6889ce838329f102f81
faraway <hzylovelyl@gmail.com>
2017-11-16 22:25:01 +0000

    feat: add helper function isVisible

---
+++ src/components/shared/isVisible.js
@@ -0,0 +1,19 @@
+export default function isVisible(el) {
+  let top = el.offsetTop
+  let left = el.offsetLeft
+  const width = el.offsetWidth
+  const height = el.offsetHeight
+
+  while (el.offsetParent) {
+    el = el.offsetParent
+    top += el.offsetTop
+    left += el.offsetLeft
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  )
+}
