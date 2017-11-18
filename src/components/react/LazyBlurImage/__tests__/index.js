07bd72f1b7b5213640a8a184d24cc648540e45e7
faraway <hzylovelyl@gmail.com>
2017-11-18 01:19:26 +0000

    feat: LazyBlurImage render without crash test complete

--- src/components/react/LazyBlurImage/__tests__/index.js
+++ src/components/react/LazyBlurImage/__tests__/index.js
@@ -3,8 +3,18 @@ import renderer from 'react-test-renderer'
 import LazyBlurImage from '../index'
 
 describe('LazyBlurImage', () => {
-  it('snake spinner render without crash', () => {
-    const component = renderer.create(<LazyBlurImage />)
+  it('LazyBlurImage renders without crash', () => {
+    const component = renderer.create(
+      <LazyBlurImage
+        thumbSource="http://bucket-1252761181.costj.myqcloud.com/desktop-mini.jpg"
+        source="http://bucket-1252761181.costj.myqcloud.com/desktop.jpg"
+        alt="hehe"
+        thumbAlt="hehe"
+        height="auto"
+        lazy
+        withBlurShadow
+      />
+    )
     const json = component.toJSON()
     expect(json).toMatchSnapshot()
   })
