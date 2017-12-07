a5fbaa2f0eb396b7a45e135ebaf9e3051d418c7f
alex <johanhu2016@gmail.com>
2017-12-07 23:52:28 +0800

    feat: add show method

--- src/components/react/ActionSheet/index.js
+++ src/components/react/ActionSheet/index.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
-import { createPortal } from 'react-dom'
+import { createPortal, render } from 'react-dom'
 import PropTypes from 'prop-types'
 
 import styles from '@style/ActionSheet/index.sass'
+
+// function noop() {}
 
 class ActionSheet extends Component {
   static propTypes = {
     operations: PropTypes.arrayOf(PropTypes.string),
     cancelButtonIndex: PropTypes.string.isRequired,
+    // callback: PropTypes.func(),
   }
 
   static defaultProps = {
-    operations: ['Operation1', 'Operation2'],
+    operations: ['Operation1', 'Operation2', 'Cancel'],
+    // callback: noop(),
   }
 
   container = null
@@ -36,6 +40,14 @@ class ActionSheet extends Component {
     }
   }
 
+  static show(config) {
+    const { cancelButtonIndex } = config
+    const actionSheet = (<ActionSheet cancelButtonIndex={cancelButtonIndex} />)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    render(actionSheet, div)
+  }
+
   render() {
     const { operations, cancelButtonIndex } = this.props
     const children = (
@@ -43,13 +55,13 @@ class ActionSheet extends Component {
         <div className={styles['button-list']}>
           {operations.map((item, index) => {
             let bitem = (
-              <div className={styles['button-list-item']}>
+              <div key={item} className={styles['button-list-item']}>
                 {item}
               </div>
             )
             if (cancelButtonIndex === index) {
               bitem = (
-                <div className={styles['cancel-button']}>
+                <div key={item} className={styles['cancel-button']}>
                   {item}
                 </div>
               )
