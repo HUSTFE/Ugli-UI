e512cc4655f35c9189bcb0250f801bf3a289c54f
JiahaiHu <892220879@qq.com>
2018-02-04 00:30:43 +0800

    feat: add style and animation

--- src/components/react/ActionSheet/index.js
+++ src/components/react/ActionSheet/index.js
@@ -1,27 +1,66 @@
 import React, { Component } from 'react'
-import { createPortal, render } from 'react-dom'
+import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 import styles from '@style/ActionSheet/index.sass'
 
-// function noop() {}
+function noop() {}
+
+let div = document.createElement('div')
+document.body.appendChild(div)
 
 class ActionSheet extends Component {
   static propTypes = {
     operations: PropTypes.arrayOf(PropTypes.string),
-    cancelButtonIndex: PropTypes.string.isRequired,
-    // callback: PropTypes.func(),
+    cancelButtonIndex: PropTypes.number,
+    // callback: PropTypes.func,
   }
 
   static defaultProps = {
     operations: ['Operation1', 'Operation2', 'Cancel'],
-    // callback: noop(),
+    cancelButtonIndex: 2,
+    // callback: () => noop,
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = this.initState()
+  }
+
+  initState = () => {
+    return {
+      active: false,
+    }
   }
 
   container = null
 
+  componentDidMount() {
+    this.enter()
+  }
+
+  enter() {
+    this.setState({
+      active: true,
+    })
+  }
+
   componentWillUnmount() {
-    this.removeContainer()
+    this.setState({
+      active: false,
+    })
+    setTimeout(() => {
+      if (this.container) {
+        this.removeContainer()
+      }
+      if (div) {
+        ReactDOM.unmountComponentAtNode(div)
+        div.parentNode.removeChild(div)
+        div = null
+      }
+    }, 300)
   }
 
   addContainer = () => {
@@ -40,38 +79,88 @@ class ActionSheet extends Component {
     }
   }
 
+  close() {
+    noop()
+    this.componentWillUnmount()
+  }
+
   static show(config) {
-    const { cancelButtonIndex } = config
-    const actionSheet = (<ActionSheet cancelButtonIndex={cancelButtonIndex} />)
-    const div = document.createElement('div')
-    document.body.appendChild(div)
-    render(actionSheet, div)
+    const actionSheet = (<ActionSheet {...config} />)
+    if (!div) {
+      div = document.createElement('div')
+      document.body.appendChild(div)
+    }
+    ReactDOM.render(actionSheet, div)
   }
 
   render() {
     const { operations, cancelButtonIndex } = this.props
-    const children = (
-      <div>
-        <div className={styles['button-list']}>
-          {operations.map((item, index) => {
-            let bitem = (
-              <div key={item} className={styles['button-list-item']}>
+    const list = (
+      <div className={styles['button-list']}>
+        {operations.map((item, index) => {
+          const itemProps = {
+            key: item,
+            className: classnames(styles['button-list-item'], {
+              [styles['cancel-button']]: cancelButtonIndex === index,
+              [styles.first]: index === 0,
+            }),
+            onTouchEnd: () => this.close(index),
+          }
+          // todo: failed to add delayStyle
+          const delayStyle = {
+            transitionDelay: 100 * index,
+            WebkitTransitionDelay: 100 * index,
+          }
+          let bitem = (
+            <div {...itemProps} style={delayStyle}>
+              {item}
+            </div>
+          )
+          if (cancelButtonIndex === index) {
+            bitem = (
+              <div {...itemProps}>
                 {item}
               </div>
             )
-            if (cancelButtonIndex === index) {
-              bitem = (
-                <div key={item} className={styles['cancel-button']}>
-                  {item}
-                </div>
-              )
-            }
-            return bitem
-          })}
-        </div>
+          }
+          return bitem
+        })}
       </div>
     )
-    return createPortal(children, this.addContainer())
+    const mask = <div className={styles.mask} onTouchEnd={() => this.close()} />
+    const children = (
+      <div className={styles['action-sheet']}>
+        <CSSTransitionGroup
+          transitionName={{
+            enter: styles['mask-enter'],
+            enterActive: styles['mask-enter-active'],
+            leave: styles['mask-leave'],
+            leaveActive: styles['mask-leave-active'],
+            appear: styles['mask-appear'],
+            appearActive: styles['mask-appear-active'],
+          }}
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          { this.state.active ? mask : null }
+        </CSSTransitionGroup>
+        <CSSTransitionGroup
+          transitionName={{
+            enter: styles['list-enter'],
+            enterActive: styles['list-enter-active'],
+            leave: styles['list-leave'],
+            leaveActive: styles['list-leave-active'],
+            appear: styles['list-appear'],
+            appearActive: styles['list-appear-active'],
+          }}
+          transitionEnterTimeout={300}
+          transitionLeaveTimeout={300}
+        >
+          { this.state.active ? list : null }
+        </CSSTransitionGroup>
+      </div>
+    )
+    return ReactDOM.createPortal(children, this.addContainer())
   }
 }
 
