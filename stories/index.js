3d7e325229211168678b803da5262d6a6eab9b5c
JiahaiHu <892220879@qq.com>
2018-02-06 18:59:57 +0800

    fix: remove code of ActionSheet

--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,6 @@ import { storiesOf } from '@storybook/react'
 // import { action } from '@storybook/addon-actions'
 // import { linkTo } from '@storybook/addon-links'
 
-import ActionSheet from '../src/components/react/ActionSheet'
 import Spinner from '../src/components/react/Indicator'
 import Button from '../src/components/react/Button/index'
 import Calendar from '../src/components/react/Calendar/index'
@@ -16,9 +15,6 @@ storiesOf('Indicator/Default(Spinner)', module)
   .add('with id', () => <Spinner id="withID" />)
 
 const uF = raw => `(UNFINISHED) ${raw}`
-
-storiesOf(uF('ActionSheet'), module)
-  .add('default', () => <button onClick={ActionSheet.show} type="button">show</button>)
 
 storiesOf(uF('Button'), module)
   .add('default', () => <Button text="UNFINISHED Button" />)
