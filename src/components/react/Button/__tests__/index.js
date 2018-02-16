7534f798dae9b1ad9b9b0ff83437bfcefad5f641
MartinNey <nielumartin@hotmail.com>
2018-02-16 11:49:31 +0800

    feat: work with ThemeProvider

--- src/components/react/Button/__tests__/index.js
+++ src/components/react/Button/__tests__/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Button from '../index'
+import ThemeProvider from '../../ThemeProvider/index'
 
 describe('Button', () => {
-  it('snake spinner render without crash', () => {
-    const component = renderer.create(<Button text="Button" />)
+  it('Basic button with text', () => {
+    const component = renderer.create(
+      <ThemeProvider startColor="green" endColor="red">
+        <Button text="Button" />
+      </ThemeProvider>,
+    )
     const json = component.toJSON()
-    expect(json).toMatchSnapshot()
+    expect(json)
+      .toMatchSnapshot()
   })
 })
--- src/components/react/Button/index.js
+++ src/components/react/Button/index.js
@@ -1,46 +1,61 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import styles from '@style/Button/index.sass'
 import { warning } from '@shared/warning'
 import { computeSize } from '@shared/size'
 
-/* eslint-disable react/prefer-stateless-function */
-// TODO: fix this lint error by changing the eslint
-// configuration or rewrite the code.
-class Button extends Component {
-  static propTypes = {
-    color: PropTypes.string,
-    size: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number,
-    ]),
-    style: PropTypes.shape(),
-    onClick: PropTypes.func,
-    text: PropTypes.string.isRequired,
-  }
-  static defaultProps = {
-    color: 'aqua',
-    size: '16',
-    style: {},
-    onClick: () => warning(true, 'onClick prop should be passed to Bottom.'),
-  }
-  render() {
-    const { color, size, style, onClick, text } = this.props
-    return (
-      <button
-        className={styles.button}
-        onClick={onClick}
-        style={{
-          height: computeSize(size),
-          color,
-          ...style,
-        }}
-      >
-        {text}
-      </button>
-    )
-  }
+/*
+const defaultTheme = {
+  startColor: '#fbc8d4',
+  endColor: '#d8a3fd',
+}
+TODO: set a shared default theme somewhere else
+There IS a default theme, but should not be applied here,
+the only thing sub-component
+*/
+// So, the priority is like: props > context > default, which means we can't just set default props simply
+// kind of disgusting...
+const Button = (props, context) => {
+  const { ctxStartColor = '#fbc8d4', ctxEndColor = '#d8a3fd' } = context
+  // TODO: warning(!useTheme, 'To get the best experience of Ugli-UI, we suggest to use component `ThemeProvider`.')
+  const { startColor = ctxStartColor, endColor = ctxEndColor, text, size, style, onClick } = props
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      style={{
+        fontSize: computeSize(size),
+        background: `linear-gradient(-45deg, ${startColor} 30%, ${endColor} 70%)`,
+        ...style,
+      }}
+    >
+      <span>{text}</span>
+    </button>
+  )
+}
+
+Button.propTypes = {
+  startColor: PropTypes.string,
+  endColor: PropTypes.string,
+  size: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  style: PropTypes.shape(),
+  onClick: PropTypes.func,
+  text: PropTypes.string.isRequired,
+}
+Button.defaultProps = {
+  startColor: undefined,
+  endColor: undefined,
+  size: '16',
+  style: {},
+  onClick: () => warning(true, 'onClick prop should be passed to Bottom.'),
+}
+Button.contextTypes = {
+  startColor: PropTypes.string,
+  endColor: PropTypes.string,
 }
 
 export default Button
